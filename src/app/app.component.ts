1366ec8afce17e74450089bb0acbb8984be3325a
skybluepro111 <TopFrontForward@mail.com>
2017-03-21 10:48:59 +0200

    add two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,23 +5,28 @@ export class Hero {
   name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input value="{{hero.name}}" placeholder="name">
-    </div>
-    `
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent  {
    title = 'Tour of Heroes'; 
-    hero: Hero = {
-      id: 1,
-      name: 'Windstorm'
-    };
+   heroes = HEROES;
 } 
